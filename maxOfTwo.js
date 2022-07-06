14277977f48374a660fa314f4c874a86d8904bab
VennyMoncivais <108498677+VennyMoncivais@users.noreply.github.com>
2022-07-06 14:42:48 -0500

    Add files via upload

---
+++ maxOfTwo.js
@@ -0,0 +1,12 @@
+let x = 1
+let y = 1
+
+// Write your code below this line.
+
+if (x>y){
+    console.log(x)
+} else if (x === y){
+    console.log('The Value are Identical')
+} else {
+    console.log(y)
+}
